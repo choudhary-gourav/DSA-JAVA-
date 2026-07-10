9b11d019811882ad924459a59589794662272489
gourav <heychoudhary.gourav@gmail.com>
2026-07-10 15:28:38 +0530

    strings

---
+++ String/accesselement.java
@@ -0,0 +1,12 @@
+package String;
+
+public class accesselement {
+    public static void main(String[] args){
+        String firstname = "Gourav";
+        String lastname = new String ("choudhary");
+       
+        System.out.println(firstname +" "+ lastname);
+        System.out.println(firstname.charAt(0));
+        System.out.println(lastname.charAt(4));
+    }
+}
