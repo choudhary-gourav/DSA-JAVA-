88c8841d52b47cabf6b38da9f6f313aabbfb3177
gourav <heychoudhary.gourav@gmail.com>
2026-07-12 17:06:49 +0530

    print every character in the string

---
+++ String/printcharacter.java
@@ -0,0 +1,10 @@
+package String;
+
+public class printcharacter {
+    public static void main(String[] args){
+        String name = "gourav";
+        for(int i = 0; i<= name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
